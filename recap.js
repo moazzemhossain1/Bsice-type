31090215cf03a7a9fe967d87ee27ba67c2319890
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-17 08:08:13 +0800

    Basice type

---
+++ recap.js
@@ -0,0 +1,8 @@
+"use strict";
+// Basic Types
+let namee = "Moazzem";
+let age = 19;
+let isStudent = true;
+console.log(namee);
+console.log(age);
+console.log(isStudent);
---
+++ recap.ts
@@ -0,0 +1,13 @@
+
+
+
+// Basic Types
+
+
+let namee: string="Moazzem";
+let age: number=19;
+let isStudent:boolean=true;
+
+console.log(namee);
+console.log(age);
+console.log(isStudent);
