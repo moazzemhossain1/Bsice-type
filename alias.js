380d8a02dc8ce21713790ed80d17c4c05c7f2ecf
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-17 08:33:22 +0800

    aliase in typeScript

---
+++ alias.js
@@ -0,0 +1,50 @@
+"use strict";
+let products = {
+    id: 1,
+    naeme: "sumsang",
+    price: 3242,
+};
+console.log(products);
+let Karime = {
+    id: 1,
+    namee: "Moazzem",
+    isPsset: true,
+};
+let abdule = {
+    id: 2,
+    namee: "Abdule",
+    isPsset: false,
+};
+let jutiy = {
+    id: 3,
+    namee: "jutay",
+    isPsset: true,
+    Deprement: "C.S.T"
+};
+let jalelUdine = {
+    id: 4,
+    namee: "JalelUdine",
+    isPsset: true,
+    Deprement: "FOD"
+};
+let akbureKhaone = {
+    id: 5,
+    namee: "Akbuere Khone",
+    isPsset: false,
+};
+let nadireKhane = {
+    id: 6,
+    namee: "Nadire Khane",
+    isPsset: true,
+};
+let MsMay = {
+    id: 7,
+    namee: "Ms : May",
+    isPsset: true,
+};
+let AsayeKhane = {
+    id: 8,
+    namee: "Ms Asaye"
+};
+console.log(nadireKhane);
+console.log(abdule);
---
+++ alias.ts
@@ -0,0 +1,96 @@
+
+
+type Produect ={
+    id:number;
+    naeme:string;
+    price:number;
+
+}
+
+let products:Produect={
+    id:1,
+    naeme:"sumsang",
+    price:3242,
+}
+
+console.log(products);
+
+
+
+
+
+
+type Student={
+    id:number;
+    namee:string;
+    isPsset?:boolean;
+    Deprement?: string;
+}
+
+
+let Karime:Student={
+    id:1,
+    namee:"Moazzem",
+    isPsset:true,
+}
+
+
+
+let abdule:Student={
+    id:2,
+    namee:"Abdule",
+    isPsset:false,
+}
+
+
+let jutiy:Student={
+    id:3,
+    namee:"jutay",
+    isPsset:true,
+    Deprement:"C.S.T"
+}
+
+
+
+let jalelUdine:Student={
+    id:4,
+    namee:"JalelUdine",
+    isPsset:true,
+    Deprement:"FOD"
+}
+
+
+
+let akbureKhaone:Student={
+    id:5,
+    namee:"Akbuere Khone",
+    isPsset:false,
+
+}
+
+
+
+let nadireKhane:Student={
+    id:6,
+    namee:"Nadire Khane",
+    isPsset:true,
+  
+}
+
+
+
+let MsMay:Student={
+    id:7,
+    namee:"Ms : May",
+    isPsset:true,
+}
+
+
+let AsayeKhane:Student={
+    id:8,
+    namee:"Ms Asaye"
+}
+
+
+console.log(nadireKhane);
+console.log(abdule)
