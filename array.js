fdce4a76dff7c11e00d1f2bd2b9b7cd3934df0ed
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-17 08:11:35 +0800

    Array degiler

---
+++ array.js
@@ -0,0 +1,5 @@
+"use strict";
+let numbers = [1, 2, 3, 4, 5];
+let frinds = ["Ali", "Balue", "Naly", "caly"];
+console.log(numbers);
+console.log(frinds);
---
+++ array.ts
@@ -0,0 +1,6 @@
+let numbers:number[]=[1,2,3,4,5];
+
+let frinds:string[]=["Ali","Balue","Naly","caly"];
+
+console.log(numbers);
+console.log(frinds)
